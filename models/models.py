ae9816f8ace78edb33391f21b6bf59875b97eaa2
Tasin <1702609@brunel.ac.uk>
2022-02-19 12:48:12 +0000

    first release

---
+++ models/models.py
@@ -0,0 +1,16 @@
+import torch
+
+def create_model(opt):
+    if opt.model == 'pix2pixHD':
+        from .pix2pixHD_model import Pix2PixHDModel
+        model = Pix2PixHDModel()
+
+
+    model.initialize(opt)
+    if opt.verbose:
+        print("model [%s] was created" % (model.name()))
+
+    if opt.isTrain and len(opt.gpu_ids):
+        model = torch.nn.DataParallel(model, device_ids=opt.gpu_ids)
+
+    return model
